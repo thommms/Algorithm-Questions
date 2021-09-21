568a600ecbaaf7b50f143747bb3a8cec34c38510
Okonkwo Thomas <thomasokonkwo91@gmail.com>
2021-09-21 16:50:54 -0500

    updated the code comment

--- MaxConsecutiveOnes.py
+++ MaxConsecutiveOnes.py
@@ -1,3 +1,18 @@
+'''
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+Example 1:
+Input: nums = [1,1,0,1,1,1]
+
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1
+
+
+Example 2:
+Input: nums = [1,0,1,1,0,1]
+Output: 2
+'''
+
 def findMax(nums):
   currMax =0
   newMax =0
@@ -8,4 +23,4 @@ def findMax(nums):
       newMax =max(currMax, newMax)
     else:
       currMax=0
-  print (newMax)
+  return (newMax)
